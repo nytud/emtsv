a36495d005d290d5e12dee6494de240a927bc33c
Sass Balint <joker@camel.nytud.hu>
2018-10-17 20:58:52 +0200

    --pipe was missing

--- mnsz2_test.sh
+++ mnsz2_test.sh
@@ -2,7 +2,11 @@
 
 date
 
-( echo "string" ; cat test_input/mnsz2_radio_100e.tok.test ) | ./trivSentSplit.sh | python3 ./emMorphREST.py | python3 ./emTagREST.py > out
+# --- ez miért nem jó? :)
+( echo "string" ; cat test_input/mnsz2_radio_100e.tok.test ) | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe > out
+
+# --- hiszen az alábbi jó... ~ 'make test-morph-tag'
+#( echo "string" ; cat test_input/puzser.test | ./trivToken.sh ) | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe
 
 date
 
