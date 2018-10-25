cc2807e57c6c23c38d44a1a4dc68d9cc6425261d
Sass Balint <joker@camel.nytud.hu>
2018-10-25 14:15:55 +0200

    trivToken.sh used -> no space in tokens :)

--- mnsz2_test.sh
+++ mnsz2_test.sh
@@ -2,11 +2,17 @@
 
 date
 
-# --- ez miért nem jó? :)
-( echo "string" ; cat test_input/mnsz2_radio_100e.tok.test ) | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe > out
+# a trivToken.sh a "január 16-án" formájú tokenek miatt kell
+# -> ti. NEM lehet szóköz tokenben! XXX :)
 
-# --- hiszen az alábbi jó... ~ 'make test-morph-tag'
-#( echo "string" ; cat test_input/puzser.test | ./trivToken.sh ) | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe
+# --- asszem, most már ez is jó! XXX :)
+#( echo "string" ; cat test_input/mnsz2_radio_100e.tok.test ) | ./trivToken.sh | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe > out
+# -> 18sec
+( echo "string" ; cat test_input/mnsz2_radio_1mio.tok.test ) | ./trivToken.sh | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe > out
+# -> 35sec után elhal... (!) XXX XXX XXX ld.: [./err.1mio] XXX XXX XXX
+
+# --- az alábbi tutira jó... ~ 'make test-morph-tag'
+#( echo "string" ; cat test_input/puzser.test | ./trivToken.sh ) | ./trivToken.sh | ./trivSentSplit.sh | python3 ./emMorphREST.py --pipe | python3 ./emTagREST.py --pipe
 
 date
 
