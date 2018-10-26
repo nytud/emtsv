63a850b15a1aa53c5c84ccaf60729ac23700a20e
Balázs Indig <indba@digitus.itk.ppke.hu>
2018-10-26 17:05:34 +0200

    Second mock-up of emTSV2.0

--- emTSV20.py
+++ emTSV20.py
@@ -38,28 +38,80 @@ def get_emdep():
 
 tools = {'morph': get_emmorph, 'pos': get_emtag, 'deptool': get_deptool, 'chunk': get_emchunk, 'dep': get_emdep}
 
+
+def build_pipeline(inp_stream, used_tools, available_tools):
+    pipeline_begin = inp_stream
+    pipeline_begin_friendly = 'HTTP POST file'
+
+    pipeline_end = pipeline_begin
+    pipeline_end_friendly = pipeline_begin_friendly
+    pipeline_prod = {'string'}
+
+    for prog in used_tools:
+        p = available_tools.get(prog)
+        if prog is not None:
+            reqv, prod, pr = p()
+            if not reqv.issubset(pipeline_prod):
+                return abort(404, Response(
+                    'ERROR: {0} program requires {1} columns but the previous program {2} has only {3} columns'.
+                    format(prog, reqv, pipeline_prod, pipeline_end_friendly)))
+            pipeline_end = process(pipeline_end, pr)
+            pipeline_prod |= prod
+
+    return pipeline_end
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:  # TODO: Implement this properly
-        pipeline_begin = open('ide2.txt', encoding='UTF-8')  # sys.stdin
-        pipeline_begin_friendly = 'STDIN'
-
-        pipeline_end = pipeline_begin
-        pipeline_end_friendly = pipeline_begin_friendly
-        pripeline_reqv = {'string'}
-        pipeline_prod = {'string'}
-
-        for prog in sys.argv[1].split(','):
-            p = tools.get(prog)
-            if prog is not None:
-                reqv, prod, pr = p()
-                if not reqv.issubset(pipeline_prod):
-                    print('ERROR: {0} program requires {1} columns but the previous program {2} has only {3} columns'.
-                          format(prog, reqv, pipeline_prod, pipeline_end_friendly))
-                    exit(1)
-                pipeline_end = process(pipeline_end, pr)
-                pipeline_prod |= prod
-
-        sys.stdout.writelines(pipeline_end)
+        sys.stdout.writelines(build_pipeline(sys.stdin, sys.argv[1].split(','), tools))
     else:
-        pass  # TODO: Same in flask. Per request build pipeline like above and stream the output...
-              # TODO: e.g. 127.0.0.1:5000/morph/pos/chunk
+        # TODO: Implement this properly
+        import codecs
+        import sys
+
+        from flask import Flask, request, Response, stream_with_context
+        from flask_restful import Api, Resource
+        from werkzeug.exceptions import abort
+
+        def add_params(restapi, _, internal_apps):
+            if internal_apps is None:
+                print('No internal_app is given!', file=sys.stderr)
+                exit(1)
+
+            kwargs = {'internal_app': internal_apps}
+            # To bypass using self and @route together
+            # TODO: / is not handled...
+            restapi.add_resource(RESTapp, '/<path:path>', resource_class_kwargs=kwargs)
+
+
+        class RESTapp(Resource):
+            def get(self, path):
+                return 'Usage: HTTP POST /tool1/tool2/tool3 e.g: {0} but suplied {1} a file' \
+                       ' mamed as \'file\' in the apropriate TSV format'.format(
+                        ' or '.join(self._internal_apps.keys()), ' and '.join(path.split('/')))
+
+            def post(self, path):
+                if 'file' not in request.files:
+                    abort(400)
+                inp_file = codecs.getreader('UTF-8')(request.files['file'])
+                last_prog = build_pipeline(inp_file, path.split('/'), self._internal_apps)
+                # TODO: This shared code wit the CLI version. Should be movet to a funciton...
+
+                return Response(
+                    stream_with_context((line.encode('UTF-8') for line in last_prog)),
+                    direct_passthrough=True)
+
+            def __init__(self, internal_app=None):
+                """
+                Init REST API class
+                :param internal_app: pre-inicialised application
+                """
+                self._internal_apps = internal_app
+                # atexit.register(self._internal_apps.__del__)  # For clean exit...
+
+        name = 'emTSV20'
+        app = Flask(name)
+        api = Api(app)
+        add_params(api, '/', tools)
+
+        app.run(debug=True)
