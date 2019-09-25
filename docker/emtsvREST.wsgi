74248dd637f524df965455194971f07472a063c4
Balázs Indig <indba@digitus.itk.ppke.hu>
2019-09-25 12:49:34 +0200

    API redesign part I: Lazy init is the new default, purge old init

--- docker/emtsvREST.wsgi
+++ docker/emtsvREST.wsgi
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
-from __init__ import init_everything, pipeline_rest_api, jnius_config, tools, presets
+from __init__ import singleton_store_factory, pipeline_rest_api, jnius_config, tools, presets
 
 jnius_config.classpath_show_warning = False  # Suppress warning
 conll_comments = False  # User can enable conll comments for compatibility or disable them for safety per request...
-inited_tools = init_everything(tools)
-application = pipeline_rest_api(name='e-magyar-tsv', available_tools=inited_tools, presets=presets,
-                                conll_comments=conll_comments)
+singleton_store = singleton_store_factory()
+application = pipeline_rest_api(name='e-magyar-tsv', available_tools=tools, presets=presets,
+                                conll_comments=conll_comments, singleton_store=singleton_store)
--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 
 import sys
 
-from __init__ import init_everything, build_pipeline, pipeline_rest_api, jnius_config, parser_skeleton, tools, presets
+from __init__ import build_pipeline, pipeline_rest_api, jnius_config, parser_skeleton, tools, presets, \
+    singleton_store_factory
 
 if __name__ == '__main__':
 
@@ -17,15 +18,11 @@ if __name__ == '__main__':
         output_iterator = opts.output_stream
 
         used_tools = opts.task[0].split(',')
-        if len(used_tools) == 1 and used_tools[0] in presets:
-            used_tools = presets[used_tools[0]]  # Resolve presets to module names to init only the needed modules...
-
-        inited_tools = init_everything({k: v for k, v in tools.items() if k in set(used_tools)})
-        output_iterator.writelines(build_pipeline(input_iterator, used_tools, inited_tools, presets, conll_comments))
+        output_iterator.writelines(build_pipeline(input_iterator, used_tools, tools, presets, conll_comments))
     elif opts.input_stream == sys.stdin or opts.output_stream == sys.stdout:
-        inited_tools = init_everything(tools)
-        app = pipeline_rest_api(name='e-magyar-tsv', available_tools=inited_tools, presets=presets,
-                                conll_comments=conll_comments)
+        singleton_store = singleton_store_factory()
+        app = pipeline_rest_api(name='e-magyar-tsv', available_tools=tools, presets=presets,
+                                conll_comments=conll_comments, singleton_store=singleton_store)
         app.run(debug=True)
     else:
         argparser.error('In REST mode, input and output are supressed, '
