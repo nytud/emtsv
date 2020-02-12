1b58fdcffb77a686856cd4b987555ea7db7801ff
mittelholcz <dev.mittelholcz@gmail.com>
2020-02-12 18:08:23 +0100

    basic test for docker done

--- tests/dtest.sh
+++ tests/dtest.sh
@@ -2,17 +2,19 @@
 
 TESTDIR=$(mktemp -d /tmp/dtestXXXXXX)
 GOLDDIR='test_output'
-MODFULL='tok,spell,morph,pos,conv-morph,dep,chunk,ner,cons,bert-ner,bert-basenp,bert-np'
+# MODFULL='tok,spell,morph,pos,conv-morph,dep,chunk,ner,cons,bert-ner,bert-basenp,bert-np'
 MODALL='tok,morph,pos,conv-morph,dep,chunk,ner'
 
 date >$TESTDIR/log
 cat test_input/input.test | \
 docker run -i --rm mtaril/emtsv:test $MODALL >$TESTDIR/all.tsv 2>>$TESTDIR/log
 
-DIFF=$(diff $GOLDDIR/all.tsv $TESTDIR/all.tsv)
+DIFF=$(git diff --no-index --color-words $GOLDDIR/all.tsv $TESTDIR/all.tsv)
 
 if [ -z "$DIFF" ] ; then
-    echo 'Docker test: OK' ;
+    echo 'Docker test passed.' ;
 else
-    git diff --no-index --color-words $GOLDDIR/all.tsv $TESTDIR/all.tsv ;
+    echo -e "$DIFF"
+    echo "Docker test failed, see the $TESTDIR/ directory."
+    exit 1
 fi
