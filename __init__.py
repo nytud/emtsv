428e1494fb6b6525f931e1e0d850808949d7633b
Balázs Indig <indba@digitus.itk.ppke.hu>
2020-04-09 01:08:45 +0200

    new release
    Bump modules to pin xtsv<1.0 (last release with OpenJDK 8 support)

--- __init__.py
+++ __init__.py
@@ -11,4 +11,4 @@ from xtsv import ModuleError, build_pipeline, pipeline_rest_api, singleton_store
 from config import tools, presets
 from download_models import download
 
-__version__ = '4.0.1'
+__version__ = '4.0.2'
