34d588336c6b0bb787090c54425fe62e56e8c261
mittelholcz <dev.mittelholcz@gmail.com>
2021-03-11 14:20:15 +0100

    testing docker: proof of concept done

--- test/dtest.sh
+++ test/dtest.sh
@@ -10,11 +10,13 @@ TESTDIR=$(mktemp -d /tmp/dtestXXXXXX)
 INPUT='test/input/alaptorveny.txt'
 GOLD='test/gold/alaptorveny_dep.tsv'
 BASENAME=$(basename $INPUT)
+MODULES='tok,morph,pos,conv-morph,dep'
+OUTPUT=""
 
 
 function find_free_port {
     # parameters: no
-    # return: free port number
+    # return: number of a free port
     res=""
     for port in {10001..15000} ; do
         (echo '' >/dev/tcp/localhost/$port) >/dev/null 2>&1 || { res=$port; break; } ;
@@ -23,70 +25,67 @@ function find_free_port {
 }
 
 
-function mydiff {
-    # parameters:
-    #    $1: mode (cli or rest)
-    #    $2: output file (file that should be compared with $GOLD)
-    # return: result of test (textual message)
-    local res="${GREEN}passed${NOCOLOR}"
-
-    if [ "$(diff $GOLD $2)" ] ; then
-        res="${RED}failed${NOCOLOR}"
-        git diff --no-index $GOLD $2 >${2%.*}.diff
-        git diff --no-index --color-words $GOLD $2 >${2%.*}.color.diff
-    fi
-    echo -e "\nDocker $1 test $res (see output files in $TESTDIR/ directory)."
+function spin {
+  local i=0
+  local sp='/-\|'
+  local n=${#sp}
+  printf ' '
+  sleep 0.1
+  while true; do
+    >&2 printf '\b%s' "${sp:i++%n:1}"
+    sleep 0.5
+  done
 }
 
-function get_output {
+
+function run_docker {
+    # Run (mtaril/emtsv:test) docker image in cli or rest mode.
     # parameters:
-    #    $1: mode (cli or xml)
-    # return: name of output file
-    OUTPUT="${TESTDIR}/${1,,}_${BASENAME%.*}".tsv
+    #    $1: mode (CLI or REST)
+    # side effects:
+    #    - write output of docker into TESTDIR library
+    #    - overwrite OUTPUT global variable
+    #    - call spinner as backgroung process
+    # return: none
+    >&2 echo -n "- $1: "
+    spin & spinpid=$!
+    disown $spinpid # https://stackoverflow.com/questions/8074904/how-to-shield-the-kill-output
+    OUTPUT="${TESTDIR}/${1,,}_${BASENAME%.*}.tsv"
     if [ "$1" = "CLI" ] ; then
-        LOG="$TESTDIR/log"
-        date >$LOG
+        date >$TESTDIR/log
         cat $INPUT | \
         docker run -i --rm mtaril/emtsv:test tok,morph,pos,conv-morph,dep >$OUTPUT 2>>$TESTDIR/log
     elif [ "$1" = "REST" ] ; then
         myport=$(find_free_port) 
         docker run --name emtsvtest -p ${myport}:5000 --rm -d mtaril/emtsv:test >/dev/null
-        curl -F "file=@$INPUT" localhost:$myport/tok/morph/pos/conv-morph/dep >$OUTPUT
+        curl -F "file=@$INPUT" localhost:$myport/tok/morph/pos/conv-morph/dep >$OUTPUT 2>/dev/null
         docker container stop emtsvtest >/dev/null
     else
         return 1
     fi
-    echo $OUTPUT
+    kill "$spinpid" #2>/dev/null
 }
 
-# echo -e $(get_output 'CLI')
-# echo -e $(get_output 'REST')
-# echo -e $(get_output 'valami')
+
+function mydiff {
+    # Compare output of docker with gold.
+    # parameters:
+    #    $1: output file (file that should be compared with $GOLD)
+    # return: result of test (textual message)
+    local res="${GREEN}✔${NOCOLOR}"
+    if [ "$(diff $GOLD $1)" ] ; then
+        res="${RED}✘${NOCOLOR} (git diff --no-index --color-words $GOLD $1)"
+    fi
+    echo -e "\b$res"
+}
+
 
 ## TEST CLI
-# echo -e '\n1. CLI'
-# OUTPUT=$(get_output 'CLI')
-# echo $OUTPUT
-# echo -e "$(mydiff 'CLI' ${OUTPUT})"
+run_docker 'CLI'
+mydiff $OUTPUT
 
-# OUTPUT="${TESTDIR}/cli_${BASENAME%.*}".tsv
-# LOG="$TESTDIR/log"
-# date >$LOG
-# cat $INPUT | \
-# docker run -i --rm mtaril/emtsv:test tok,morph,pos,conv-morph,dep >$OUTPUT 2>>$TESTDIR/log
-# echo -e "$(mydiff 'CLI' ${OUTPUT})"
+# ## TEST REST API
+run_docker 'REST'
+mydiff $OUTPUT
 
-
-## TEST REST API
-echo -e '\n2. REST API'
-OUTPUT=$(get_output 'REST')
-echo $OUTPUT
-# echo -e "$(mydiff 'REST' ${OUTPUT})"
-
-# OUTPUT="${TESTDIR}/rest_${BASENAME%.*}".tsv
-
-# myport=$(find_free_port) 
-# docker run --name emtsvtest -p ${myport}:5000 --rm -d mtaril/emtsv:test >/dev/null
-# curl -F "file=@$INPUT" localhost:$myport/tok/morph/pos/conv-morph/dep >$OUTPUT
-# docker container stop emtsvtest >/dev/null
-# echo -e "$(mydiff 'REST API' ${OUTPUT})"
+echo -e "See output files in $TESTDIR/ directory."
