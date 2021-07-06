dfa63b268f6df4f13a20273c3be60289aef351be
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2021-07-06 15:11:59 +0200

    Experimental support for variable number of processes.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cat /app/docker/uwsgi.ini.template | sed -e "s/EMTSV_NUM_PROCESSES/${EMTSV_NUM_PROCESSES}/" > /app/docker/uwsgi.ini
+
 if [ -z "$@" ]; then
     uwsgi --ini /app/docker/uwsgi.ini --die-on-term
 else
