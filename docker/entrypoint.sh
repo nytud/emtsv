0be7552dd385b60d21de267960bd75fe583eab0f
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2021-08-10 14:25:07 +0200

    Got rid of the template stuff from entrypoint.sh.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-cat /app/docker/uwsgi.ini.template | sed -e "s/EMTSV_NUM_PROCESSES/${EMTSV_NUM_PROCESSES}/" > /app/docker/uwsgi.ini
-
 if [ -z "$@" ]; then
     uwsgi --ini /app/docker/uwsgi.ini --die-on-term
 else
